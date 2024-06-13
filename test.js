73f49be251d1b5d7aff1706c41581ccc66acc84f
Nicholas Kokotakis <njk51@georgetown.edu>
2024-06-13 10:39:46 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,32 @@
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    };
+};
+
+const favoriteNumber = 42;
+
+const instructor1 = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+};
+
+const instructor2 = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  };
+
+  function createAnimal(species, verb, noise){
+    return {
+      species,
+      [verb](){
+        return noise;
+      }
+    };
+  };
